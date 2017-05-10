71949712b0059480c0b9b531b3e3f556d19ecad2
gp22 <gp22@tutanota.com>
2017-05-10 10:47:56 +0700

    add insert

--- linkedList/insert.c
+++ linkedList/insert.c
@@ -26,9 +26,38 @@ void Insert(int data, int n)
     head = temp1;
     return;
   }
-  // For all other cases we will need to go to the (n-1)th node.
-  
-};
+  /*
+  For all other cases we will need to go to the (n-1)th node.
+  To do this we need to start at the head.
+  */
+  struct Node* temp2 = head;
+  /*
+  This for loop will iterate through each node in the list n-2 times,
+  which will end up at the (n-1)th node.
+  */
+  for (int i = 0; i < n-2; i++)
+  {
+    temp2 = temp2->next;
+  }
+  /*
+  The next step is to set the link field of the newly created node (temp1) as
+  the link of the (n-1)th node (temp2), and then adjust the link of the (n-1)th
+  node (temp2) to point to the newly created node (temp1);
+  */
+  temp1->next = temp2->next;
+  temp2->next = temp1;
+}
+
+void Print()
+{
+  struct Node* temp = head;
+  while (temp != NULL)
+  {
+    printf("%d", temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+}
 
 /* -------------------------------- Main -------------------------------- */
 
@@ -37,6 +66,6 @@ int main() {
   Insert(2, 1); // List: 2
   Insert(3, 2); // List: 2, 3
   Insert(4, 1); // List: 4, 2, 3
-  Insert(5, 2); // List: 2, 5, 2, 3
+  Insert(5, 2); // List: 4, 5, 2, 3
   Print();
 }
