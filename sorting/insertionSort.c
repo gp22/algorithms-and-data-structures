589c80aa2d3a15935c0f97d8cde757546852d01a
gp22 <gp22@tutanota.com>
2017-05-20 22:44:05 +0700

    add insertion sort

--- sorting/insertionSort.c
+++ sorting/insertionSort.c
@@ -1,34 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int sort(int a[], int length)
-{
-  for (int j = 1; j < length; j++)
-  {
+/* ------------------------- Function Definitions ------------------------- */
+
+// Insertion sort algorithm.
+int sort(int a[], int length) {
+  for (int j = 1; j < length; j++) {
     int key = a[j];
     int i = j - 1;
-    while (i >= 0 && a[i] > key)
-    {
+    while (i >= 0 && a[i] > key) {
       a[i + 1] = a[i];
-      i = i - 1;
+      i -= 1;
     }
     a[i + 1] = key;
   }
   return *a;
 }
 
-void print(int a[], int length)
-{
-  for (int i = 0; i < length; i++)
-  {
+// Print all elements in an integer array of length.
+void print(int a[], int length) {
+  for (int i = 0; i < length; i++) {
     printf("%d ", a[i]);
   }
   printf("\n");
 }
 
+/* -------------------------------- Main -------------------------------- */
+
 int main() {
 
-  int array[6] = {6, 2, 4, 5, 1, 3};
+  int array[6] = {1, 2, 4, 5, 6, 3};
 
   print(array, 6);
   sort(array, 6);
